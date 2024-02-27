3f5f07a53fa01d1a7a1ea3d56571ffc1d25f5fb8
Kmag20 <owenmawira@gmail.com>
2024-02-27 14:13:18 +0300

    New Node

--- binary_trees.h
+++ binary_trees.h
@@ -11,7 +11,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s 
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
@@ -22,6 +22,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
-binary_tree_t* binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
