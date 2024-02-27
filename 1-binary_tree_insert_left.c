c16442ea94068246e10361af7e68ae3404da0a84
Kmag20 <owenmawira@gmail.com>
2024-02-27 16:11:42 +0300

    Insert left;

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		left_node->left = parent->left;
 	}
 
-	parent->left = new;
+	parent->left = left_node;
 
 	return (left_node);
 }
