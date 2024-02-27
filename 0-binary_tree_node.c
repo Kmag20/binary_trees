f12926f0c79eacdcb60baca4ad18a69ecfbe78ae
Kmag20 <owenmawira@gmail.com>
2024-02-27 14:00:55 +0300

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,18 +3,20 @@
 
 /**
  * binary_tree_node - creates a singular binary tree node
- * 
+ *
  * @parent: Pointer to the created node
  * @value: value of the new node
- * 
+ *
  * Return: Pointer to the newly created node
 */
-binary_tree_t* binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t* node = malloc(sizeof(binary_tree_t));
+	binary_tree_t* node;
+       
+	node = malloc(sizeof(binary_tree_t));
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
-	return node;
+	return (node);
 }
