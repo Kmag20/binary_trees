c2cb8fd6f63fb145c3c77f73d860e1c751defe5b
Kmag20 <owenmawira@gmail.com>
2024-02-28 13:19:02 +0300

    Diliti

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,27 +7,23 @@
  * @tree - pointer to the root node of the entire tree
  * Return: Returns nothing
  */
-void binary_tree_delete(binary_tree_t *tree) {
-    if (tree != NULL) {
-        binary_tree_t *next_left = tree->left;
-        binary_tree_t *next_right = tree->right;
-        free(tree);
-        binary_tree_delete(next_left);
-        binary_tree_delete(next_right);
-        return;
-    } 
-    else {
-        return;
-    }
-} 
-  /*if (tree->left == NULL && tree->right == NULL) {
-    free(tree);
-    return;
-  }
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *next_left;
+	binary_tree_t *next_right;
 
-  binary_tree_delete(tree->left);
-  binary_tree_delete(tree->right);
-  
-  free(tree);*/
-  
+	if (tree != NULL)
+	{
+		next_left = tree->left;
+		next_right = tree->right;
 
+		free(tree);
+		binary_tree_delete(next_left);
+		binary_tree_delete(next_right);
+		return;
+	}
+	else
+	{
+		return;
+	}
+}
