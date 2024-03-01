39bea76eb9cd5bbf1d6f5903c90af45296a7676a
Kmag20 <owenmawira@gmail.com>
2024-03-01 12:58:34 +0300

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,19 +8,18 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL) {
+	if (tree == NULL)
+	{
 		return (0);
 	}
 
-
-	if (tree->left || tree->right) {
+	if (tree->left || tree->right)
+	{
 		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	}
 
-	else {
+	else
+	{
 		return (0);
 	}
-	
-	
-
 }
